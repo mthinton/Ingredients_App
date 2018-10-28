2f4da33272b7e99799e26f362d4f958f24d21403
Matthew Hinton <mthinton@gmail.com>
2018-10-28 16:16:21 -0400

    added heroku server

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                       'mongodb://Matthew:ingredients1@ds143953.mlab.com:43953/ingredients_users'
 
 
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = 'https://intense-garden-96611.herokuapp.com/';
